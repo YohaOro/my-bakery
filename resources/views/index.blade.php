d9368727fb1af6006e29c67ebd1cfadcb4e0e469
Gastón Nicolás Muñiz <gaston.muniz@mercadolibre.com>
2020-01-22 17:32:06 -0300

    SAVEPOINT

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,12 @@
 
 @section('title', 'Titulo')
 
+@section('stylesheet')
+    @parent
+
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}" >
+@endsection
+
 @section('content')
     <h1>Index</h1>
 @endsection
---
+++ resources/views/products.blade.php
@@ -0,0 +1,15 @@
+@extends('base')
+
+@section('title', 'Titulo')
+
+@section('stylesheet')
+    @parent
+
+    <link rel="stylesheet" href="css/products.css" >
+@endsection
+
+@section('content')
+    <link rel="stylesheet" href="css/products.css" >
+
+    <h1>Productos</h1>
+@endsection
